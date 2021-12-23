e633a5c7cc9a1b5405dbff94b2e655c6da46a670
Chandan Rai <nckr2014@gmail.com>
2021-12-23 20:09:36 +0530

    initial commit

--- src/main/java/in/name/chandan/OneToManyBidirectionalMappingApplication.java
+++ src/main/java/in/name/chandan/OneToManyBidirectionalMappingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnetomanybidirectionalmappingApplication {
+public class OneToManyBidirectionalMappingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OnetomanybidirectionalmappingApplication.class, args);
+        SpringApplication.run(OneToManyBidirectionalMappingApplication.class, args);
     }
 
 }
